86030e5bdb397844362c60b50971275a3e010ff7
alozanoblanquicettt <alberto.lozano@logismart.com.co>
2025-06-10 15:50:37 -0500

    migraciones

--- back/database/migrations/2025_05_22_235217_ordenes_ejecutadas.php
+++ back/database/migrations/2025_05_22_235217_ordenes_ejecutadas.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->string('maestra_fases_fk')->nullable();
             $table->string('maestra_tipo_acondicionamiento_fk')->nullable();
             $table->string('linea_produccion')->nullable();
+            $table->string('proceso')->nullable()->default('eject');
+            $table->string('estado')->nullable()->default('100');
             $table->timestamps();
         });
     }
