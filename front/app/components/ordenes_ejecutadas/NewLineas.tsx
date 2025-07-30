192ee33bb7f1fff17017ed5468049ec9b2cbf78e
alozanoblanquicettt <159572929+alozanoblanquicettt@users.noreply.github.com>
2025-07-30 10:21:31 -0500

    estado de la oden

--- front/app/components/ordenes_ejecutadas/NewLineas.tsx
+++ front/app/components/ordenes_ejecutadas/NewLineas.tsx
@@ -53,7 +53,11 @@ const NewLineas = () => {
     const lista_procesos = Array.isArray(lista.linea_procesos) ? lista.linea_procesos : [];
     const lista_fases = Array.isArray(lista.linea_fases) ? lista.linea_fases : [];
 
-    // if (lista_procesos.length === 0 && lista_fases.length === 0) {
+    if (orden.estado === 11500) {
+        window.close();
+    }
+    console.log("orden", orden.estado);
+    // if (lista_procesos.length === 0 && lista_fases.length === 0 && orden === null) {
     //     window.close();
     // }
     
