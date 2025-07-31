571af04e551ce85d4509d2e3f3380dcf5f10b4ac
alozanoblanquicettt <159572929+alozanoblanquicettt@users.noreply.github.com>
2025-07-30 21:50:50 -0500

    migraciones_ conciliaciones

---
+++ back/database/migrations/2025_05_22_235217_conciliaciones.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('conciliaciones', function (Blueprint $table) {
+            $table->id();
+            $table->string('orden_ejecutada')->nullable();
+            $table->string('adaptation_date_id')->nullable();
+            $table->string('number_order')->nullable();
+            $table->string('descripcion_maestra')->nullable();
+
+            // articulos a conciliar
+            $table->string('codart')->nullable();
+            $table->string('codesart')->nullable();
+            $table->double('quantityToProduce')->nullable();  // (a)
+            $table->double('faltante')->nullable(); // (b)
+            $table->double('adicionales')->nullable(); // (c)
+            $table->double('rechazo')->nullable(); // (d)
+            $table->double('danno_proceso')->nullable(); // (e)
+            $table->double('devolucion')->nullable(); // (f)
+            $table->double('sobrante')->nullable(); // (g)
+            $table->double('total')->nullable(); // =a + c + g - (b + d + e + f)
+            $table->double('rendimiento')->nullable(); // =h - e / ( a - (d + b) ) * 100
+
+            // Obtener usuario
+            $table->string('user')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('conciliaciones');
+    }
+};
---
+++ front/app/components/ordenes_ejecutadas/NewConsolida.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewConsolida = () => {
+    return (
+        <div>
+            <h1>NewConsolida</h1>
+        </div>
+    )
+}
+
+export default NewConsolida
