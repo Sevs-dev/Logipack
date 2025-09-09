7fbf337fb4a394cf2709705888afdb8fb8d6287f
Sevs-dev <sebastian.ramirez@logismart.com.co>
2025-09-09 15:19:10 -0500

    cambio

--- front/app/config/api.ts
+++ front/app/config/api.ts
@@ -1,2 +1,2 @@
 export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
-export const ENVIRONMENT = 'development';
+export const ENVIRONMENT: 'development' | 'production' | 'test' = 'development';
